73e7d318d680e39f702c8678108a0312f43dfa7e
sabbir <programmarsabbir@gmail.com>
2026-06-29 17:47:02 +0800

    working with my orders page

--- src/pages/Dashboard/Customer/MyOrders.jsx
+++ src/pages/Dashboard/Customer/MyOrders.jsx
@@ -8,7 +8,7 @@ import { HiOutlineShoppingBag, HiOutlineClipboardList } from "react-icons/hi";
 
 const MyOrders = () => {
   const { user } = useAuth();
-  const axiosSecure = useAxiosSecure();
+  // const axiosSecure = useAxiosSecure();
 
   const {
     data: orders = [],
@@ -17,12 +17,14 @@ const MyOrders = () => {
   } = useQuery({
     queryKey: ["orders", user?.email],
     queryFn: async () => {
-      const result = await axiosSecure(`/my-orders/${user.email}`);
-      return result.data;
+      // const result = await axiosSecure(`/my-orders/${user.email}`);
+      const result = await fetch(`https://backend-kappa-two-21.vercel.app/my-orders/${user.email}`);
+      return result.json();
     },
     enabled: !!user?.email,
   });
 
+  console.log("My orders", orders);
   if (isLoading) return <LoadingSpinner />;
 
   return (
