4442342665c99524cabb8aaff0d64081b4aa25b9
sabbir <programmarsabbir@gmail.com>
2026-06-29 17:25:39 +0800

    hello

--- src/StripePayment/PaymentSuccess.jsx
+++ src/StripePayment/PaymentSuccess.jsx
@@ -7,10 +7,19 @@ import { BsArrowRight } from "react-icons/bs";
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
   const sessionId = searchParams.get("session_id");
-
+//OLD 
+  // useEffect(() => {
+  //   if (sessionId) {
+  //     axios.post(`${import.meta.env.VITE__URL}/payment-success`, {
+  //       sessionId,
+  //     });
+  //   }
+  // }, [sessionId]);
+  
+  //try to problem solve 
   useEffect(() => {
     if (sessionId) {
-      axios.post(`${import.meta.env.VITE_API_URL}/payment-success`, {
+      axios.post("https://bookcourier12.vercel.app/payment-success", {
         sessionId,
       });
     }
